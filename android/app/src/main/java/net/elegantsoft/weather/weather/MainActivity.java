741b74cf3540e0d274a4d9749f4392a48a799b90
ElegantSoft= <react.laravel.dev@gmail.com>
2020-06-24 09:24:24 +0200

    finish app

---
+++ android/app/src/main/java/net/elegantsoft/weather/weather/MainActivity.java
@@ -0,0 +1,6 @@
+package net.elegantsoft.weather.weather;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/ui/MyHomePage.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import '../api/LocationApi.dart';
+import '../api/MapApi.dart';
+import '../model/WeatherData.dart';
+import '../ui/Weather.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  WeatherData _weatherData;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentLocation();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.lightBlue,
+      appBar: new AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: new Text(widget.title),
+      ),
+      body: _weatherData != null
+          ? Weather(weatherData: _weatherData)
+          : Center(
+              child: CircularProgressIndicator(
+                strokeWidth: 4.0,
+                valueColor: AlwaysStoppedAnimation(Colors.white),
+              ),
+            ),
+    );
+  }
+
+  getCurrentLocation() async {
+    LocationApi locationApi = LocationApi.getInstance();
+    final location = await locationApi.getLocation();
+    loadWeather(lat: location.lat, lon: location.lon);
+  }
+
+  loadWeather({double lat, double lon}) async {
+    MapApi mapApi = MapApi.getInstance();
+    final data = await mapApi.getWeather(lat: lat, lon: lon);
+    setState(() {
+      this._weatherData = data;
+    });
+  }
+}
